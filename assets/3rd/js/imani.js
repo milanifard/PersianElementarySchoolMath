b5b49f1f4d61992977c2e8c418da45cd68f0e031
zeynabimani <zeynab.s.imani@gmail.com>
2020-12-04 18:04:09 +0330

    add half of page 18 js

--- assets/3rd/js/imani.js
+++ assets/3rd/js/imani.js
@@ -349,3 +349,259 @@ function correct_p17_q2() {
         q2_3_2.classList.add("red_shadow");
     }
 }
+
+function correct_p18_q2() {
+    select6 = document.getElementById("s6")
+    select7 = document.getElementById("s7")
+    select8 = document.getElementById("s8")
+    select9 = document.getElementById("s9")
+    select10 = document.getElementById("s10")
+    select11 = document.getElementById("s11")
+    select12 = document.getElementById("s12")
+    select13 = document.getElementById("s13")
+    select14 = document.getElementById("s14")
+    select15 = document.getElementById("s15")
+    select16 = document.getElementById("s16")
+    select17 = document.getElementById("s17")
+    select18 = document.getElementById("s18")
+    select19 = document.getElementById("s19")
+    select20 = document.getElementById("s20")
+    select21 = document.getElementById("s21")
+    select22 = document.getElementById("s22")
+    select23 = document.getElementById("s23")
+    select24 = document.getElementById("s24")
+
+    c6 = select6.classList;
+    c7 = select7.classList;
+    c8 = select8.classList;
+    c9 = select9.classList;
+    c10 = select10.classList;
+    c11 = select11.classList;
+    c12 = select12.classList;
+    c13 = select13.classList;
+    c14 = select14.classList;
+    c15 = select15.classList;
+    c16 = select16.classList;
+    c17 = select17.classList;
+    c18 = select18.classList;
+    c19 = select19.classList;
+    c20 = select20.classList;
+    c21 = select21.classList;
+    c22 = select22.classList;
+    c23 = select23.classList;
+    c24 = select24.classList;
+
+    document.getElementById("hour6").classList = ''
+    document.getElementById("hour7").classList = ''
+    document.getElementById("hour8").classList = ''
+    document.getElementById("hour9").classList = ''
+    document.getElementById("hour10").classList = ''
+    document.getElementById("hour11").classList = ''
+    document.getElementById("hour12").classList = ''
+    document.getElementById("hour13").classList = ''
+    document.getElementById("hour14").classList = ''
+    document.getElementById("hour15").classList = ''
+    document.getElementById("hour16").classList = ''
+    document.getElementById("hour17").classList = ''
+    document.getElementById("hour18").classList = ''
+    document.getElementById("hour19").classList = ''
+    document.getElementById("hour20").classList = ''
+    document.getElementById("hour21").classList = ''
+    document.getElementById("hour22").classList = ''
+    document.getElementById("hour23").classList = ''
+    document.getElementById("hour24").classList = ''
+
+    if (c6.length == 0) {
+        document.getElementById("hour6").classList.add("empty")
+    } else if (c6[0] == "sleep") {
+        document.getElementById("hour6").classList.add("correct")
+    } else {
+        document.getElementById("hour6").classList.add("wrong")
+    }
+
+    if (c7.length == 0) {
+        document.getElementById("hour7").classList.add("empty")
+    } else if (c7[0] == "pray_morning") {
+        document.getElementById("hour7").classList.add("correct")
+    } else {
+        document.getElementById("hour7").classList.add("wrong")
+    }
+
+    if (c8.length == 0) {
+        document.getElementById("hour8").classList.add("empty")
+    } else if (c8[0] == "shower") {
+        document.getElementById("hour8").classList.add("correct")
+    } else {
+        document.getElementById("hour8").classList.add("wrong")
+    }
+
+    if (c9.length == 0) {
+        document.getElementById("hour9").classList.add("empty")
+    } else if (c9[0] == "study") {
+        document.getElementById("hour9").classList.add("correct")
+    } else {
+        document.getElementById("hour9").classList.add("wrong")
+    }
+
+    if (c10.length == 0) {
+        document.getElementById("hour10").classList.add("empty")
+    } else if (c10[0] == "study") {
+        document.getElementById("hour10").classList.add("correct")
+    } else {
+        document.getElementById("hour10").classList.add("wrong")
+    }
+
+    if (c11.length == 0) {
+        document.getElementById("hour11").classList.add("empty")
+    } else if (c11[0] == "rest") {
+        document.getElementById("hour11").classList.add("correct")
+    } else {
+        document.getElementById("hour11").classList.add("wrong")
+    }
+
+    if (c12.length == 0) {
+        document.getElementById("hour12").classList.add("empty")
+    } else if (c12[0] == "rest") {
+        document.getElementById("hour12").classList.add("correct")
+    } else {
+        document.getElementById("hour12").classList.add("wrong")
+    }
+
+    if (c13.length == 0) {
+        document.getElementById("hour13").classList.add("empty")
+    } else if (c13[0] == "pray_day") {
+        document.getElementById("hour13").classList.add("correct")
+    } else {
+        document.getElementById("hour13").classList.add("wrong")
+    }
+
+    if (c14.length == 0) {
+        document.getElementById("hour14").classList.add("empty")
+    } else if (c14[0] == "study") {
+        document.getElementById("hour14").classList.add("correct")
+    } else {
+        document.getElementById("hour14").classList.add("wrong")
+    }
+
+    if (c15.length == 0) {
+        document.getElementById("hour15").classList.add("empty")
+    } else if (c15[0] == "study") {
+        document.getElementById("hour15").classList.add("correct")
+    } else {
+        document.getElementById("hour15").classList.add("wrong")
+    }
+
+    if (c16.length == 0) {
+        document.getElementById("hour16").classList.add("empty")
+    } else if (c16[0] == "way_to_party") {
+        document.getElementById("hour16").classList.add("correct")
+    } else {
+        document.getElementById("hour16").classList.add("wrong")
+    }
+
+    if (c17.length == 0) {
+        document.getElementById("hour17").classList.add("empty")
+    } else if (c17[0] == "party") {
+        document.getElementById("hour17").classList.add("correct")
+    } else {
+        document.getElementById("hour17").classList.add("wrong")
+    }
+
+    if (c18.length == 0) {
+        document.getElementById("hour18").classList.add("empty")
+    } else if (c18[0] == "party") {
+        document.getElementById("hour18").classList.add("correct")
+    } else {
+        document.getElementById("hour18").classList.add("wrong")
+    }
+
+    if (c19.length == 0) {
+        document.getElementById("hour19").classList.add("empty")
+    } else if (c19[0] == "party") {
+        document.getElementById("hour19").classList.add("correct")
+    } else {
+        document.getElementById("hour19").classList.add("wrong")
+    }
+
+    if (c20.length == 0) {
+        document.getElementById("hour20").classList.add("empty")
+    } else if (c20[0] == "party") {
+        document.getElementById("hour20").classList.add("correct")
+    } else {
+        document.getElementById("hour20").classList.add("wrong")
+    }
+
+    if (c21.length == 0) {
+        document.getElementById("hour21").classList.add("empty")
+    } else if (c21[0] == "way_to_home") {
+        document.getElementById("hour21").classList.add("correct")
+    } else {
+        document.getElementById("hour21").classList.add("wrong")
+    }
+
+    if (c22.length == 0) {
+        document.getElementById("hour22").classList.add("empty")
+    } else if (c22[0] == "pray_night") {
+        document.getElementById("hour22").classList.add("correct")
+    } else {
+        document.getElementById("hour22").classList.add("wrong")
+    }
+
+    if (c23.length == 0) {
+        document.getElementById("hour23").classList.add("empty")
+    } else if (c23[0] == "sleep") {
+        document.getElementById("hour23").classList.add("correct")
+    } else {
+        document.getElementById("hour23").classList.add("wrong")
+    }
+
+    if (c24.length == 0) {
+        document.getElementById("hour24").classList.add("empty")
+    } else if (c24[0] == "sleep") {
+        document.getElementById("hour24").classList.add("correct")
+    } else {
+        document.getElementById("hour24").classList.add("wrong")
+    }
+}
+
+function erase_p18_q2() {
+    document.getElementById("s6").classList = ''
+    document.getElementById("s7").classList = ''
+    document.getElementById("s8").classList = ''
+    document.getElementById("s9").classList = ''
+    document.getElementById("s10").classList = ''
+    document.getElementById("s11").classList = ''
+    document.getElementById("s12").classList = ''
+    document.getElementById("s13").classList = ''
+    document.getElementById("s14").classList = ''
+    document.getElementById("s15").classList = ''
+    document.getElementById("s16").classList = ''
+    document.getElementById("s17").classList = ''
+    document.getElementById("s18").classList = ''
+    document.getElementById("s19").classList = ''
+    document.getElementById("s20").classList = ''
+    document.getElementById("s21").classList = ''
+    document.getElementById("s22").classList = ''
+    document.getElementById("s23").classList = ''
+    document.getElementById("s24").classList = ''
+
+    document.getElementById("hour6").classList = ''
+    document.getElementById("hour7").classList = ''
+    document.getElementById("hour8").classList = ''
+    document.getElementById("hour9").classList = ''
+    document.getElementById("hour10").classList = ''
+    document.getElementById("hour11").classList = ''
+    document.getElementById("hour12").classList = ''
+    document.getElementById("hour13").classList = ''
+    document.getElementById("hour14").classList = ''
+    document.getElementById("hour15").classList = ''
+    document.getElementById("hour16").classList = ''
+    document.getElementById("hour17").classList = ''
+    document.getElementById("hour18").classList = ''
+    document.getElementById("hour19").classList = ''
+    document.getElementById("hour20").classList = ''
+    document.getElementById("hour21").classList = ''
+    document.getElementById("hour22").classList = ''
+    document.getElementById("hour23").classList = ''
+    document.getElementById("hour24").classList = ''
+}
