e23ddc85bfc4f9b906e65f3375c870d2f6adec90
Parsa <salehipour.edu@gmail.com>
2020-12-10 18:24:35 +0330

    tangram moving added

--- assets/3rd/sass/parsasal/tangram_script.js
+++ assets/3rd/sass/parsasal/tangram_script.js
@@ -1,95 +1,42 @@
-const body = document.body;
-const container = document.querySelector(".container");
-const pl = document.querySelector(".canvas.p");
-const tl1 = document.querySelector(".canvas.t-l-1");
-const tl2 = document.querySelector(".canvas.t-l-2");
-const ts1 = document.querySelector(".canvas.t-s-1");
-const ts2 = document.querySelector(".canvas.t-s-2");
-const sq = document.querySelector(".canvas.s");
-const tm = document.querySelector(".canvas.t-m");
+$(document).ready(function () {
 
-// degree state closure
-const rotationIncrementor = function () {
-  let rotation = 0;
+  $('.block').draggable({
+    containment: 'window',
+    stack: '.block',
+    snap: true,
+    snapMode: 'outer',
+    snapTolerance: 13,
+  });
 
-  function updateRotation() {
-    rotation = rotation + 45 < 360 ? rotation + 45 : 0;
-  }
+  $('#blockTray').on('mousedown', function () {
+    $('#instruction').fadeOut('slow');
+  });
 
-  return {
-    rotate: function () {
-      updateRotation();
-    },
-    getDegree: function () {
-      return rotation;
-    } };
+  // Make blocks rotate 90 deg on each click
+  var angle = 90;
 
-};
+  $('.block').click(function () {
+    if ($(this).attr('id') === "parallelogram") {
+      console.log("angle is :"+angle)
+      let skew_percent =  ((angle/180)%2) ? -1 : +1 ;
+      // let skew_percent =  -1
+      console.log("skew is :"+skew_percent *45)
+      $(this).css({
 
-// initialise state for each shape
-let plRotator = rotationIncrementor();
-let tl1Rotator = rotationIncrementor();
-let tl2Rotator = rotationIncrementor();
-let ts1Rotator = rotationIncrementor();
-let ts2Rotator = rotationIncrementor();
-let sqRotator = rotationIncrementor();
-let tmRotator = rotationIncrementor();
+        '-webkit-transform': 'rotate(' + angle + 'deg) skew('+skew_percent*45+'deg)',
+        '-moz-transform': 'rotate(' + angle + 'deg) skew('+skew_percent*45+'deg)',
+        '-o-transform': 'rotate(' + angle + 'deg) skew('+skew_percent*45+'deg)'
+      });
+    } else {
+      $(this).css({
+        '-webkit-transform': 'rotate(' + angle + 'deg)',
+        '-moz-transform': 'rotate(' + angle + 'deg)',
+        '-o-transform': 'rotate(' + angle + 'deg)',
+        '-ms-transform': 'rotate(' + angle + 'deg)'
+      });
+    }
 
-// click on shape makes rotation
-pl.ondblclick = function () {
-  plRotator.rotate();
-  pl.style.transform = `rotate(${plRotator.getDegree()}deg)`;
-};
-tl1.ondblclick = function () {
-  tl1Rotator.rotate();
-  tl1.style.transform = `rotate(${tl1Rotator.getDegree()}deg)`;
-};
-tl2.ondblclick = function () {
-  tl2Rotator.rotate();
-  tl2.style.transform = `rotate(${tl2Rotator.getDegree()}deg)`;
-};
-ts1.ondblclick = function () {
-  ts1Rotator.rotate();
-  ts1.style.transform = `rotate(${ts1Rotator.getDegree()}deg)`;
-};
-ts2.ondblclick = function () {
-  ts2Rotator.rotate();
-  ts2.style.transform = `rotate(${ts2Rotator.getDegree()}deg)`;
-};
-tl1.ondblclick = function () {
-  tl1Rotator.rotate();
-  tl1.style.transform = `rotate(${tl1Rotator.getDegree()}deg)`;
-};
-sq.ondblclick = function () {
-  sqRotator.rotate();
-  sq.style.transform = `rotate(${sqRotator.getDegree()}deg)`;
-};
-tm.ondblclick = function () {
-  tmRotator.rotate();
-  tm.style.transform = `rotate(${tmRotator.getDegree()}deg)`;
-};
+    angle += 90;
+  });
 
-//initialise displacejs with shapes
-let displacejsOptions = {
-  constrain: true,
-  relativeTo: body,
-  onMouseDown: dragActive,
-  onTouchStart: dragActive,
-  onMouseUp: dragInactive,
-  onTouchStop: dragInactive };
-
-
-function dragActive(el) {
-  el.className += ' active';
-}
-function dragInactive(el) {
-  el.className = el.className.replace('active', '');
-}
-
-window.displacejs(tl1, displacejsOptions);
-window.displacejs(tl2, displacejsOptions);
-window.displacejs(ts1, displacejsOptions);
-window.displacejs(ts2, displacejsOptions);
-window.displacejs(pl, displacejsOptions);
-window.displacejs(sq, displacejsOptions);
-window.displacejs(tm, displacejsOptions);
+});
