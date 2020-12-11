2710174b6f1d44c8c3ba9042069c8ea52a7a00c2
MohannaAnsari <ansari.m0512@gmail.com>
2020-12-12 02:41:31 +0330

    page 13 almost

---
+++ assets/3rd/js/Ansari.js
@@ -0,0 +1,35 @@
+function p13ques1() {
+  check("p13q1_1",8)
+}
+
+function p13ques2() {
+  check("p13q2_1",16)
+  check("p13q2_2",22)
+  check("p13q2_3",30)
+  check("p13q2_4",10)
+
+}
+//  function check(id,c){
+//   x = document.getElementById(id).value;
+//   if (x == c) {
+//       console.log("ok");
+//       correct(id);
+//     } else {
+//       console.log("Not ok");
+//       notCorrect(id);
+//     }
+//  }
+
+// function correct(id){
+//     document.getElementById(id).style.background = "lightgreen";
+//   }
+
+// function notCorrect(id){
+//     document.getElementById(id).style.background = "#e45f5f";
+//   }
+
+
+ 
+
+
+  
