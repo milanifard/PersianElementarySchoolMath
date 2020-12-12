bb8eb63f6612294dfd2dee444d756eed2ebe53a3
Parastoo.Bagherpour <parastoo.bagherpour@partsoftware.com>
2020-12-13 00:47:21 +0330

    taaamoli p22

--- assets/3rd/js/bagherpour.js
+++ assets/3rd/js/bagherpour.js
@@ -27,56 +27,327 @@ function p22answerQ2(){
     }
 }
 
-function algooShomaresh(){
-    for(i=1 ; i<8; i++){
-        console.log(i);
-        if(document.getElementById(`tamrin1avali${i}`) == null)
-        document.getElementById(`tamrin1avali${i}`).style.color = "blue";
-        console.log(document.getElementById(`tamrin1avali${i}`).style.color);
-    }
+function algooShomaresh11(id , value){
     
-    if (document.getElementById("tamrin1avali1").value == 20){
-        document.getElementById("tamrin1avali1").style.color = "green";
+    if (value == 20){
+        makeGreen(id);
     }
     else{
-        document.getElementById("tamrin1avali1").style.color = "red";
+        makeRed(id);
     }
-    if (document.getElementById("tamrin1avali2").value == 24){
-        document.getElementById("tamrin1avali2").style.color = "green";
-    }
-    else{
-        document.getElementById("tamrin1avali2").style.color = "red";
-    }
-    if (document.getElementById("tamrin1avali3").value == 28){
-        document.getElementById("tamrin1avali3").style.color = "green";
+    if (document.getElementById("practice1first2").value == 24){
+        document.getElementById("practice1first2").style.color = "green";
     }
     else{
-        document.getElementById("tamrin1avali3").style.color = "red";
+        document.getElementById("practice1first2").style.color = "red";
     }
-    if (document.getElementById("tamrin1avali4").value == 32){
-        document.getElementById("tamrin1avali4").style.color = "green";
-    }
-    else{
-        document.getElementById("tamrin1avali4").style.color = "red";
-    }
-    if (document.getElementById("tamrin1avali5").value == 36){
-        document.getElementById("tamrin1avali5").style.color = "green";
+    if (document.getElementById("practice1first3").value == 28){
+        document.getElementById("practice1first3").style.color = "green";
     }
     else{
-        document.getElementById("tamrin1avali5").style.color = "red";
+        document.getElementById("practice1first3").style.color = "red";
     }
-    if (document.getElementById("tamrin1avali6").value == 40){
-        document.getElementById("tamrin1avali6").style.color = "green";
-    }
-    else{
-        document.getElementById("tamrin1avali6").style.color = "red";
-    }
-    if (document.getElementById("tamrin1avali7").value == 44){
-        document.getElementById("tamrin1avali7").style.color = "green";
+    if (document.getElementById("practice1first4").value == 32){
+        document.getElementById("practice1first4").style.color = "green";
     }
     else{
-        document.getElementById("tamrin1avali7").style.color = "red";
+        document.getElementById("practice1first4").style.color = "red";
+    }
+    if (document.getElementById("practice1first5").value == 36){
+        document.getElementById("practice1first5").style.color = "green";
+    }
+    else{
+        document.getElementById("practice1first5").style.color = "red";
+    }
+    if (document.getElementById("practice1first6").value == 40){
+        document.getElementById("practice1first6").style.color = "green";
+    }
+    else{
+        document.getElementById("practice1first6").style.color = "red";
+    }
+    if (document.getElementById("practice1first7").value == 44){
+        document.getElementById("practice1first7").style.color = "green";
+    }
+    else{
+        document.getElementById("practice1first7").style.color = "red";
     }
 
 
 }
+
+function algooShomaresh12(){
+    if (document.getElementById("practice1second1").value == 24){
+        document.getElementById("practice1second1").style.color = "green";
+    }
+    else{
+        document.getElementById("practice1second1").style.color = "red";
+    }
+    if (document.getElementById("practice1second2").value == 30){
+        document.getElementById("practice1second2").style.color = "green";
+    }
+    else{
+        document.getElementById("practice1second2").style.color = "red";
+    }
+    if (document.getElementById("practice1second3").value == 36){
+        document.getElementById("practice1second3").style.color = "green";
+    }
+    else{
+        document.getElementById("practice1second3").style.color = "red";
+    }
+    if (document.getElementById("practice1second4").value == 42){
+        document.getElementById("practice1second4").style.color = "green";
+    }
+    else{
+        document.getElementById("practice1second4").style.color = "red";
+    }
+    if (document.getElementById("practice1second5").value == 48){
+        document.getElementById("practice1second5").style.color = "green";
+    }
+    else{
+        document.getElementById("practice1second5").style.color = "red";
+    }
+    if (document.getElementById("practice1second6").value == 54){
+        document.getElementById("practice1second6").style.color = "green";
+    }
+    else{
+        document.getElementById("practice1second6").style.color = "red";
+    }
+    if (document.getElementById("practice1second7").value == 60){
+        document.getElementById("practice1second7").style.color = "green";
+    }
+    else{
+        document.getElementById("practice1second7").style.color = "red";
+    }
+    if (document.getElementById("practice1second8").value == 66){
+        document.getElementById("practice1second8").style.color = "green";
+    }
+    else{
+        document.getElementById("practice1second8").style.color = "red";
+    }
+ }
+
+ function algooShomaresh13(){
+    if (document.getElementById("practice1third1").value == 12){
+        document.getElementById("practice1third1").style.color = "green";
+    }
+    else{
+        document.getElementById("practice1third1").style.color = "red";
+    }
+    if (document.getElementById("practice1third2").value == 24){
+        document.getElementById("practice1third2").style.color = "green";
+    }
+    else{
+        document.getElementById("practice1third2").style.color = "red";
+    }
+    if (document.getElementById("practice1third3").value == 36){
+        document.getElementById("practice1third3").style.color = "green";
+    }
+    else{
+        document.getElementById("practice1third3").style.color = "red";
+    }
+ }
+ function algooShomaresh21(){
+    if (document.getElementById("practice2first1").value == 0){
+        document.getElementById("practice2first1").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2first1").style.color = "red";
+    }
+    if (document.getElementById("practice2first2").value == 5){
+        document.getElementById("practice2first2").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2first2").style.color = "red";
+    }
+    if (document.getElementById("practice2first3").value == 10){
+        document.getElementById("practice2first3").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2first3").style.color = "red";
+    }
+    if (document.getElementById("practice2first4").value == 15){
+        document.getElementById("practice2first4").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2first4").style.color = "red";
+    }
+    if (document.getElementById("practice2first5").value == 20){
+        document.getElementById("practice2first5").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2first5").style.color = "red";
+    }
+    if (document.getElementById("practice2first6").value == 25){
+        document.getElementById("practice2first6").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2first6").style.color = "red";
+    }
+    if (document.getElementById("practice2first7").value == 30){
+        document.getElementById("practice2first7").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2first7").style.color = "red";
+    }
+    if (document.getElementById("practice2first8").value == 35){
+        document.getElementById("practice2first8").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2first8").style.color = "red";
+    }
+ }
+
+ function algooShomaresh22(){
+    if (document.getElementById("practice2second1").value == 12){
+        document.getElementById("practice2second1").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2second1").style.color = "red";
+    }
+    if (document.getElementById("practice2second2").value == 14){
+        document.getElementById("practice2second2").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2second2").style.color = "red";
+    }
+    if (document.getElementById("practice2second3").value == 16){
+        document.getElementById("practice2second3").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2second3").style.color = "red";
+    }
+    if (document.getElementById("practice2second4").value == 18){
+        document.getElementById("practice2second4").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2second4").style.color = "red";
+    }
+    if (document.getElementById("practice2second5").value == 20){
+        document.getElementById("practice2second5").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2second5").style.color = "red";
+    }
+    if (document.getElementById("practice2second6").value == 22){
+        document.getElementById("practice2second6").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2second6").style.color = "red";
+    }
+    if (document.getElementById("practice2second7").value == 24){
+        document.getElementById("practice2second7").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2second7").style.color = "red";
+    }
+ }
+
+ function algooShomaresh23(){
+    if (document.getElementById("practice2third1").value == 3){
+        document.getElementById("practice2third1").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2third1").style.color = "red";
+    }
+    if (document.getElementById("practice2third2").value == 6){
+        document.getElementById("practice2third2").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2third2").style.color = "red";
+    }
+    if (document.getElementById("practice2third3").value == 9){
+        document.getElementById("practice2third3").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2third3").style.color = "red";
+    }
+    if (document.getElementById("practice2third4").value == 12){
+        document.getElementById("practice2third4").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2third4").style.color = "red";
+    }
+    if (document.getElementById("practice2third5").value == 15){
+        document.getElementById("practice2third5").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2third5").style.color = "red";
+    }
+    if (document.getElementById("practice2third6").value == 18){
+        document.getElementById("practice2third6").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2third6").style.color = "red";
+    }
+    if (document.getElementById("practice2third7").value == 21){
+        document.getElementById("practice2third7").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2third7").style.color = "red";
+    }
+ }
+
+ function algooShomaresh24(){
+    if (document.getElementById("practice2forth1").value == 0){
+        document.getElementById("practice2forth1").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2forth1").style.color = "red";
+    }
+    if (document.getElementById("practice2forth2").value == 7){
+        document.getElementById("practice2forth2").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2forth2").style.color = "red";
+    }
+    if (document.getElementById("practice2forth3").value == 14){
+        document.getElementById("practice2forth3").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2forth3").style.color = "red";
+    }
+    if (document.getElementById("practice2forth4").value == 21){
+        document.getElementById("practice2forth4").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2forth4").style.color = "red";
+    }
+    if (document.getElementById("practice2forth5").value == 28){
+        document.getElementById("practice2forth5").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2forth5").style.color = "red";
+    }
+    if (document.getElementById("practice2forth6").value == 35){
+        document.getElementById("practice2forth6").style.color = "green";
+    }
+    else{
+        document.getElementById("practice2forth6").style.color = "red";
+    }
+ }
+ function plus(){
+    if (document.getElementById("practice31").value == 25){
+        document.getElementById("practice31").style.color = "green";
+    }
+    else{
+        document.getElementById("practice31").style.color = "red";
+    }
+    if (document.getElementById("practice32").value == 20){
+        document.getElementById("practice32").style.color = "green";
+    }
+    else{
+        document.getElementById("practice32").style.color = "red";
+    }
+    if (document.getElementById("practice33").value == 28){
+        document.getElementById("practice33").style.color = "green";
+    }
+    else{
+        document.getElementById("practice33").style.color = "red";
+    }
+ }
+ function makeGreen(id){    
+    document.getElementById(id).style.color = "green";
+ }
+ function makeRed(id){    
+    document.getElementById(id).style.color = "green";
+ }
