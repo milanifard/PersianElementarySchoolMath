518ce40408bee92353867aec48da21b2f8ee6cf5
MohannaAnsari <ansari.m0512@gmail.com>
2020-12-13 02:00:04 +0330

    14,15 all done, 13 canvas img

--- assets/3rd/js/Ansari.js
+++ assets/3rd/js/Ansari.js
@@ -54,8 +54,44 @@ function p14ques3() {
 
 }
 
+function p15ques1() {
+  check("p15q1_1",9)
+
+  check("p15q2_1",25)
+  check("p15q2_2",30)
+  check("p15q2_3",35)
+  check("p15q2_4",40)
+  check("p15q2_5",45)
+  check("p15q2_6",50)
+  check("p15q2_7",55)
+  check("p15q2_8",60)
+
+}
 
 
+
+function p15ques2() {
+  check("p15q3_1_1",15)
+
+  check("p15q3_1",35)
+  check("p15q3_2",55)
+
+  check("p15q4_1",25)
+  check("p15q4_2",45)
+  check("p15q4_3",30)
+
+  check("p15q5_1_1",5)
+  check("p15q5_1_2",15)
+
+  check("p15q5_1",40)
+  check("p15q5_2",45)
+  check("p15q6_1",35)
+  check("p15q6_2",30)
+
+
+
+
+}
  
 
 
