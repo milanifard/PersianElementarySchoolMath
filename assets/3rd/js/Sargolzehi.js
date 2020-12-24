2c515dad3fcd8779279628ee4ff793bb83e5f742
Hojat Sargolzehi <hojats7731@gmail.com>
2020-12-24 15:31:33 +0330

    add page 80 - add js file - update css file - add images

---
+++ assets/3rd/js/Sargolzehi.js
@@ -0,0 +1,48 @@
+// Page 80 - Question 1
+function checkQ1Answer() {
+    let ansField = document.getElementById("p80-q1-answer");
+    let ans = ansField.value;
+    let response = document.getElementById("p80-q1-response");
+    if (ans === "36" || ans === "۳۶") {
+        response.textContent = "آفرین، درست جواب دادی!";
+        response.style.color = '#005301';
+        ansField.style.backgroundColor = '#c9ff63';
+    } else {
+        response.textContent = "اشتباه بود، بیشتر دقت کن!";
+        response.style.color = '#ff0000';
+        ansField.style.backgroundColor = '#ff8c8c';
+    }
+}
+
+// Page 80 - Question 2
+function checkQ2Answer() {
+    let ansField = document.getElementById("p80-q2-answer");
+    let ans = ansField.value;
+    let response = document.getElementById("p80-q2-response");
+    if (ans === "18" || ans === "۱۸") {
+        response.textContent = "آفرین، درست جواب دادی!";
+        response.style.color = '#005301';
+        ansField.style.backgroundColor = '#c9ff63';
+    } else {
+        response.textContent = "اشتباه بود، بیشتر دقت کن!";
+        response.style.color = '#ff0000';
+        ansField.style.backgroundColor = '#ff8c8c';
+    }
+}
+
+// Page 80 - Question 3
+function checkQ3Answer() {
+    let ansField = document.getElementById("p80-q3-answer");
+    let ans = ansField.value;
+    let response = document.getElementById("p80-q3-response");
+    if (ans === "25" || ans === "۲۵") {
+        response.textContent = "آفرین، درست جواب دادی!";
+        response.style.color = '#005301';
+        ansField.style.backgroundColor = '#c9ff63';
+    } else {
+        response.textContent = "اشتباه بود، بیشتر دقت کن!";
+        response.style.color = '#ff0000';
+        ansField.style.backgroundColor = '#ff8c8c';
+    }
+}
+
