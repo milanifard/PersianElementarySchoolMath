3e41bb3cfef309d2e4dc2d28a5e2779cbe6b2389
yaldakheirkhah <yal.kheirkhah@gmail.com>
2020-12-24 17:14:55 +0330

    master

--- assets/3rd/js/KheirKhah.js
+++ assets/3rd/js/KheirKhah.js
@@ -9,12 +9,14 @@ function p62ques1() {
   }
 
   function p62ques3() {
-    check("reason",'همان عدد')
+    check("checklist","correct")
     x2 = document.getElementById("p62ques3_2_b").value;
     x3 = document.getElementById("p62ques3_3_b").value;
+    // if()
     check("p62ques3_2_a",x2)
     check("p62ques3_3_a",x3)
   }
+  
 
   function p63ques1() {
     check("p63ques1_1_a",4)
@@ -46,7 +48,8 @@ function p62ques1() {
 
  function check(id,c){
   x = document.getElementById(id).value;
-  if (x == c) {
+  console.log(x)
+  if ((x == c) && (x!='')) {
       console.log("ok");
       correct(id);
     } else {
@@ -59,9 +62,68 @@ function p62ques1() {
     document.getElementById(id).style.background = "lightgreen";
   }
 
-  function notCorrect(id){
+function notCorrect(id){
     document.getElementById(id).style.background = "#e45f5f";
   }
- 
 
+
+ 
+  function chcol(id , colorcode,bordcolorcode){
+    document.getElementById(id).style.background = colorcode;
+    document.getElementById(id).style.color = colorcode;
+    document.getElementById(id).style.borderColor =bordcolorcode ;
+
+
+  }
   
+
+// pagination
+function getCurrentPageFromURL() {
+  /*
+  This function extracts page number from url
+  */
+  let numberPattern = /\d+/g;
+  let url_numbers = window.location.href.match(numberPattern);
+  return parseInt(url_numbers[url_numbers.length - 1])
+}
+let current_page = getCurrentPageFromURL()
+console.log("current page : " + current_page)
+
+
+function prevPage() {
+  if (current_page > 1) {
+      current_page--;
+      changePage(current_page);
+  }
+}
+
+function nextPage() {
+  current_page++;
+  changePage(current_page);
+}
+
+function changePage(page) {
+
+  window.location.href = `./book_3_${page}.html`;
+
+}
+
+function loadPageChanger() {
+  document.querySelector(".page-number input").value = current_page
+}
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  const input = document.querySelector(".page-number input")
+  input.addEventListener('keypress', changePageOnEnter);
+
+  function changePageOnEnter(e) {
+      if (e.key === "Enter") {
+          changePage(input.value)
+      }
+  }
+  document.querySelector(".prev-page,.prev").addEventListener("click", prevPage);
+  document.querySelector(".next-page,.next").addEventListener("click", nextPage);
+
+  loadPageChanger();
+});
