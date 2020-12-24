67a813bd7a9218b661489dfa51cac728d44e8e06
yaldakheirkhah <yal.kheirkhah@gmail.com>
2020-12-24 16:02:04 +0330

    Last commit I hope (63 not finished)

--- assets/3rd/js/KheirKhah.js
+++ assets/3rd/js/KheirKhah.js
@@ -62,7 +62,7 @@ function p62ques1() {
     document.getElementById(id).style.background = "lightgreen";
   }
 
-  function notCorrect(id){
+function notCorrect(id){
     document.getElementById(id).style.background = "#e45f5f";
   }
 
@@ -70,3 +70,54 @@ function p62ques1() {
  
 
   
+
+// pagination
+function getCurrentPageFromURL() {
+  /*
+  This function extracts page number from url
+  */
+  let numberPattern = /\d+/g;
+  let url_numbers = window.location.href.match(numberPattern);
+  return parseInt(url_numbers[url_numbers.length - 1])
+}
+let current_page = getCurrentPageFromURL()
+console.log("current page : " + current_page)
+
+
+function prevPage() {
+  if (current_page > 1) {
+      current_page--;
+      changePage(current_page);
+  }
+}
+
+function nextPage() {
+  current_page++;
+  changePage(current_page);
+}
+
+function changePage(page) {
+
+  window.location.href = `./book_3_${page}.html`;
+
+}
+
+function loadPageChanger() {
+  document.querySelector(".page-number input").value = current_page
+}
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  const input = document.querySelector(".page-number input")
+  input.addEventListener('keypress', changePageOnEnter);
+
+  function changePageOnEnter(e) {
+      if (e.key === "Enter") {
+          changePage(input.value)
+      }
+  }
+  document.querySelector(".prev-page,.prev").addEventListener("click", prevPage);
+  document.querySelector(".next-page,.next").addEventListener("click", nextPage);
+
+  loadPageChanger();
+});
