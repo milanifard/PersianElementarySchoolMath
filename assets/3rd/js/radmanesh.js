6b1c61d0f2dbfce091ee4109c73c02099b261749
Shiva <shiva.radmanesh7888@gmail.com>
2020-12-25 05:30:11 +0330

    final

--- assets/3rd/js/radmanesh.js
+++ assets/3rd/js/radmanesh.js
@@ -341,3 +341,357 @@ function q3_68(a, b, returnedValue){
         document.getElementById(id).style.borderBottomColor = "red"
     }
 }
+
+
+function q1_69(){
+    if (document.getElementById("1-69-2-2").value == 4)
+        document.getElementById("1-69-2-2").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-2-2").style.backgroundColor = "red"
+        document.getElementById("1-69-2-2").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-2-3").value == 6)
+        document.getElementById("1-69-2-3").style.backgroundColor = "#76ff03"
+    else
+    {
+        document.getElementById("1-69-2-3").style.backgroundColor = "red"
+        document.getElementById("1-69-2-3").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-2-4").value == 8)
+        document.getElementById("1-69-2-4").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-2-4").style.backgroundColor = "red"
+        document.getElementById("1-69-2-4").style.color = "white"
+    }
+
+    if (document.getElementById("1-69-2-5").value == 10)
+        document.getElementById("1-69-2-5").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-2-5").style.backgroundColor = "red"
+        document.getElementById("1-69-2-5").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-2-6").value == 12)
+        document.getElementById("1-69-2-6").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-2-6").style.backgroundColor = "red"
+        document.getElementById("1-69-2-6").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-2-7").value == 14)
+        document.getElementById("1-69-2-7").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-2-7").style.backgroundColor = "red"
+        document.getElementById("1-69-2-7").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-2-8").value == 16)
+        document.getElementById("1-69-2-8").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-2-8").style.backgroundColor = "red"
+        document.getElementById("1-69-2-8").style.color = "white"
+    }
+
+    if (document.getElementById("1-69-2-9").value == 18)
+        document.getElementById("1-69-2-9").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-2-9").style.backgroundColor = "red"
+        document.getElementById("1-69-2-9").style.color = "white"
+    }
+
+    // -----------------------------------------
+    if (document.getElementById("1-69-3-2").value == 6)
+        document.getElementById("1-69-3-2").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-3-2").style.backgroundColor = "red"
+        document.getElementById("1-69-3-2").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-3-3").value == 9)
+        document.getElementById("1-69-3-3").style.backgroundColor = "#76ff03"
+    else
+    {
+        document.getElementById("1-69-3-3").style.backgroundColor = "red"
+        document.getElementById("1-69-3-3").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-3-4").value == 12)
+        document.getElementById("1-69-3-4").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-3-4").style.backgroundColor = "red"
+        document.getElementById("1-69-3-4").style.color = "white"
+    }
+
+    if (document.getElementById("1-69-3-5").value == 15)
+        document.getElementById("1-69-3-5").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-3-5").style.backgroundColor = "red"
+        document.getElementById("1-69-3-5").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-3-6").value == 18)
+        document.getElementById("1-69-3-6").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-3-6").style.backgroundColor = "red"
+        document.getElementById("1-69-3-6").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-3-7").value == 21)
+        document.getElementById("1-69-3-7").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-3-7").style.backgroundColor = "red"
+        document.getElementById("1-69-3-7").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-3-8").value == 18)
+        document.getElementById("1-69-3-8").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-3-8").style.backgroundColor = "red"
+        document.getElementById("1-69-3-8").style.color = "white"
+    }
+
+    if (document.getElementById("1-69-3-9").value == 27)
+        document.getElementById("1-69-3-9").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-3-9").style.backgroundColor = "red"
+        document.getElementById("1-69-3-9").style.color = "white"
+    }
+
+    // -----------------------------
+
+    if (document.getElementById("1-69-4-2").value == 8)
+        document.getElementById("1-69-4-2").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-4-2").style.backgroundColor = "red"
+        document.getElementById("1-69-4-2").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-4-3").value == 12)
+        document.getElementById("1-69-4-3").style.backgroundColor = "#76ff03"
+    else
+    {
+        document.getElementById("1-69-4-3").style.backgroundColor = "red"
+        document.getElementById("1-69-4-3").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-4-4").value == 16)
+        document.getElementById("1-69-4-4").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-4-4").style.backgroundColor = "red"
+        document.getElementById("1-69-4-4").style.color = "white"
+    }
+
+    if (document.getElementById("1-69-4-5").value == 20)
+        document.getElementById("1-69-4-5").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-4-5").style.backgroundColor = "red"
+        document.getElementById("1-69-4-5").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-4-6").value == 24)
+        document.getElementById("1-69-4-6").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-4-6").style.backgroundColor = "red"
+        document.getElementById("1-69-4-6").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-4-7").value == 28)
+        document.getElementById("1-69-4-7").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-4-7").style.backgroundColor = "red"
+        document.getElementById("1-69-4-7").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-4-8").value == 32)
+        document.getElementById("1-69-4-8").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-4-8").style.backgroundColor = "red"
+        document.getElementById("1-69-4-8").style.color = "white"
+    }
+
+    if (document.getElementById("1-69-4-9").value == 36)
+        document.getElementById("1-69-4-9").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-4-9").style.backgroundColor = "red"
+        document.getElementById("1-69-4-9").style.color = "white"
+    }
+
+// --------------------
+
+
+    if (document.getElementById("1-69-5-2").value == 10)
+        document.getElementById("1-69-5-2").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-5-2").style.backgroundColor = "red"
+        document.getElementById("1-69-5-2").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-5-3").value == 15)
+        document.getElementById("1-69-5-3").style.backgroundColor = "#76ff03"
+    else
+    {
+        document.getElementById("1-69-5-3").style.backgroundColor = "red"
+        document.getElementById("1-69-5-3").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-5-4").value == 20)
+        document.getElementById("1-69-5-4").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-5-4").style.backgroundColor = "red"
+        document.getElementById("1-69-5-4").style.color = "white"
+    }
+
+    if (document.getElementById("1-69-5-5").value == 25)
+        document.getElementById("1-69-5-5").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-5-5").style.backgroundColor = "red"
+        document.getElementById("1-69-5-5").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-5-6").value == 30)
+        document.getElementById("1-69-5-6").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-5-6").style.backgroundColor = "red"
+        document.getElementById("1-69-5-6").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-5-7").value == 35)
+        document.getElementById("1-69-5-7").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-5-7").style.backgroundColor = "red"
+        document.getElementById("1-69-5-7").style.color = "white"
+    }
+
+
+    if (document.getElementById("1-69-5-8").value == 40)
+        document.getElementById("1-69-5-8").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-5-8").style.backgroundColor = "red"
+        document.getElementById("1-69-5-8").style.color = "white"
+    }
+
+    if (document.getElementById("1-69-5-9").value == 45)
+        document.getElementById("1-69-5-9").style.backgroundColor = "#76ff03"
+    else{
+        document.getElementById("1-69-5-9").style.backgroundColor = "red"
+        document.getElementById("1-69-5-9").style.color = "white"
+    }
+}
+
+
+function q2_69(){
+    if(document.getElementById("q2-69-top-c").value * document.getElementById("q2-69-top-l").value == 24 &&
+       document.getElementById("q2-69-top-r").value == 24) {
+
+        document.getElementById("q2-69-top-c").style.borderBottomColor = "#76ff03"
+        document.getElementById("q2-69-top-l").style.borderBottomColor = "#76ff03"
+        document.getElementById("q2-69-top-r").style.borderBottomColor = "#76ff03"
+    }
+    else {
+        document.getElementById("q2-69-top-c").style.borderBottomColor = "red"
+        document.getElementById("q2-69-top-l").style.borderBottomColor = "red"
+        document.getElementById("q2-69-top-r").style.borderBottomColor = "red"
+    }
+
+
+    if(document.getElementById("q2-69-center-c").value * document.getElementById("q2-69-center-l").value == 24 &&
+    document.getElementById("q2-69-top-r").value == 24) {
+
+     document.getElementById("q2-69-center-c").style.borderBottomColor = "#76ff03"
+     document.getElementById("q2-69-center-l").style.borderBottomColor = "#76ff03"
+     document.getElementById("q2-69-center-r").style.borderBottomColor = "#76ff03"
+    }
+    else {
+        document.getElementById("q2-69-center-c").style.borderBottomColor = "red"
+        document.getElementById("q2-69-center-l").style.borderBottomColor = "red"
+        document.getElementById("q2-69-center-r").style.borderBottomColor = "red"
+    }
+
+
+    if(document.getElementById("q2-69-bottom-c").value * document.getElementById("q2-69-bottom-l").value == 24 &&
+       document.getElementById("q2-69-bottom-r").value == 24) {
+
+     document.getElementById("q2-69-bottom-c").style.borderBottomColor = "#76ff03"
+     document.getElementById("q2-69-bottom-l").style.borderBottomColor = "#76ff03"
+     document.getElementById("q2-69-bottom-r").style.borderBottomColor = "#76ff03"
+    }
+    else {
+        document.getElementById("q2-69-bottom-c").style.borderBottomColor = "red"
+        document.getElementById("q2-69-bottom-l").style.borderBottomColor = "red"
+        document.getElementById("q2-69-bottom-r").style.borderBottomColor = "red"
+    }
+
+}
+
+
+function q3_69(enterdValue){
+    if(enterdValue == 10){
+        document.getElementById('q3-69').style.borderBottomColor = "#76ff03"
+    }
+    else {
+        document.getElementById('q3-69').style.borderBottomColor = "red"
+    }
+}
+
+function q4_69(enterdValue){
+    if(enterdValue == 12){
+        document.getElementById('q4-69').style.borderBottomColor = "#76ff03"
+    }
+    else {
+        document.getElementById('q4-69').style.borderBottomColor = "red"
+    }
+}
+
+
+function q5_69(enterdValue){
+    if(enterdValue == 12){
+        document.getElementById('q5-69').style.borderBottomColor = "#76ff03"
+    }
+    else {
+        document.getElementById('q5-69').style.borderBottomColor = "red"
+    }
+}
+
+
+function q6_69(enterdValue){
+    if(enterdValue == 30){
+        document.getElementById('q6-69').style.borderBottomColor = "#76ff03"
+    }
+    else {
+        document.getElementById('q6-69').style.borderBottomColor = "red"
+    }
+}
+
+
+function qcal_69(enterdValue){
+    if(enterdValue == 96){
+        document.getElementById('qcal-69').style.borderBottomColor = "#76ff03"
+    }
+    else {
+        document.getElementById('qcal-69').style.borderBottomColor = "red"
+    }
+}
+
+
+
