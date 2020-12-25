c4f086aec8d409c646dce1b274823ca8ebbe4155
mammad <hisa71117@gmail.com>
2020-12-25 05:40:42 +0330

    bugFixed:page130

--- assets/3rd/js/mohajer/mohajer.js
+++ assets/3rd/js/mohajer/mohajer.js
@@ -3,3 +3,9 @@ function enterPage(ele) {
       location.replace(`book_3_${ele.value}.html`)
     }
 }
+ function checkInput(element, correct) {
+    var value = element.value;
+    if (value == correct)
+      element.style.color = 'green'
+    else element.style.color = 'red';
+  }
