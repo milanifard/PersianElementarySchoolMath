fa770fa915f98c5f8bd7827235d92e214ecae0e2
hamed-PC <hamedgoharshad@gmail.com>
2020-12-26 15:39:03 +0330

    - 120 page ui done

--- assets/3rd/js/Goharshad.js
+++ assets/3rd/js/Goharshad.js
@@ -13,3 +13,39 @@ function check_value(elem, val) {
         elem.style.background = notCorrect;
     }
 }
+
+function q1_118(element) {
+    switch (element.id) {
+        case "Q1_text_box_1":
+        case "Q1_text_box_2":
+            if (element.value === "") {
+                element.style.backgroundColor = transparent_color;
+            } else if (element.value === "2") {
+                element.style.backgroundColor = correct_table_color;
+            } else {
+                element.style.backgroundColor = incorrect_table_color;
+            }
+            break;
+        case "Q1_text_box_3":
+            if (element.value === "") {
+                element.style.backgroundColor = transparent_color;
+            } else if (element.value === "3") {
+                element.style.backgroundColor = correct_table_color;
+            } else {
+                element.style.backgroundColor = incorrect_table_color;
+            }
+            break;
+        case "Q1_text_box_4":
+            if (element.value === "") {
+                element.style.backgroundColor = transparent_color;
+            } else if (element.value === "4") {
+                element.style.backgroundColor = correct_table_color;
+            } else {
+                element.style.backgroundColor = incorrect_table_color;
+            }
+            break;
+    }
+}
+
+function q1_118_1(element){
+}
